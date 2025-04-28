f06f8a2551a2e20c21084d77df551b1895f95018
Owen Halvorson <ohalv049@uottawa.ca>
2025-04-28 16:23:37 -0400

    Commiting first blcok.py version

---
+++ block.py
@@ -0,0 +1,17 @@
+import time 
+import hashlib
+
+class Block: 
+    def _init_(self, index, transactions, timestamp, previous_hash, nonce=0, validator=None):
+        self.index=index
+        self.transactions=transactions
+        self.timestamp=timestamp
+        self.previous_hash=previous_hash
+        self.nonce=nonce
+        self.validator=validator
+
+        self.hash=self.compute_hash()
+
+    def compute_hash(self):
+        block_string = f"{self.index}{self.transactions}{self.timestamp}{self.previous_hash}{self.nonce}{self.validator}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
